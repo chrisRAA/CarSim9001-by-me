300bff9b8e56d4bc1455df4caf744ab34a94e3c0
chris <christofferraahauge@gmail.com>
2022-09-29 11:15:18 +0200

    Indtil videre lavet Tank og noget af Wheel

--- model.py
+++ model.py
@@ -4,7 +4,13 @@ class Car(object):
     pass
 
 class Wheel(object):
-    pass
+    def __init__(self):
+        self.orientation = randint(0, 360)
+
+    def rotate(self, revolutions):
+        degreesOfRotation = 360 * revolutions
+        self.orientation = (self.orientation + degreesOfRotation) % 360
+
 
 class Engine(object):
     pass
@@ -13,4 +19,15 @@ class Gearbox(object):
     pass
 
 class Tank(object):
-    pass
+
+    def __init__(self):
+        self.capacity = 100
+        self.contents = 100
+
+    def remove(self, amount):
+        self.contents -= amount
+        if self.contents < 0:
+            self.contents = 0
+
+    def refuel(self):
+        self.contents = self.capacity
